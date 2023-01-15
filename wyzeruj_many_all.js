f0a73162a03a5faefaadc86bee3d644d6652800c
wojtish <20014605+wojtish@users.noreply.github.com>
2023-01-15 12:05:21 +0100

    Update wyzeruj_many_all.js

--- wyzeruj_many_all.js
+++ wyzeruj_many_all.js
@@ -6,10 +6,10 @@ var l = bib.lenght;
 if(entry().field("many all") == l){
 entry().set("wyzeruj","📕")
 };
-if(entry().field("many all") != l) && (entry().field("many all") < (entry().field("procenty_many_all",51)){
+if(entry().field("many all") != l) && (entry().field("many all") < (entry().field("procenty_many_all",51){
 entry().set("wyzeruj","📗");
 };
-if(entry().field("many all") != l) && (entry().field("many all") > (entry().field("procenty_many_all",50)){
+if(entry().field("many all") != l) && (entry().field("many all") > (entry().field("procenty_many_all",50){
 entry().set("wyzeruj","📙");
 };
 
