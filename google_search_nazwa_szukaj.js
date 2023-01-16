e39730d87d940bc00d893f6ec589c5b582a1dd86
wojtish <20014605+wojtish@users.noreply.github.com>
2023-01-16 07:02:07 +0100

    Update and rename google_search_nazwa_szukaj.js to google_search_set.js

--- google_search_nazwa_szukaj.js
+++
@@ -1,7 +0,0 @@
-// google_search_nazwa_szukaj.js
-
-intview.data("https://www.google.com/search?q=" + " " + entry().field("nazwa") + " " + entry().field("tag_szukaj_google"))
-intview.send()
-
-
-
---
+++ google_search_set.js
@@ -0,0 +1,3 @@
+// google_search_set.js
+
+entry().set("google search", "https://www.google.com/search?q=" + entry().field("nazwa") + " " + entry().field("tag_szukaj_google"))
