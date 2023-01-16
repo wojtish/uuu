afc8f465debb014d9f9a910292fe4c14d90771ce
wojtish <20014605+wojtish@users.noreply.github.com>
2023-01-16 06:49:32 +0100

    Update google_search_nazwa_szukaj.js

--- google_search_nazwa_szukaj.js
+++ google_search_nazwa_szukaj.js
@@ -1,4 +1,7 @@
 // google_search_nazwa_szukaj.js
 
-intview.data("https://www.google.com/search?q=(entry().field("nazwa") + " " + entry().field("tag_szukaj_google")))
+intview.data("https://www.google.com/search?q=" + " " + entry().field("nazwa") + " " + entry().field("tag_szukaj_google"))
 intview.send()
+
+
+
