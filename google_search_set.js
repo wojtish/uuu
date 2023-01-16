950712e977b0f13e60bebc1b135de27fe106f6bd
wojtish <20014605+wojtish@users.noreply.github.com>
2023-01-16 07:12:02 +0100

    Update google_search_set.js

--- google_search_set.js
+++ google_search_set.js
@@ -1,3 +1,3 @@
 // google_search_set.js
 
-entry().set("szukaj google", "https://www.google.com/search?q=" + entry().field("nazwa") + " " + entry().field("tag_szukaj_google"))
+entry().set("szukaj google", "https://www.google.com/search?q=" + entry().field("nazwa") + " " + entry().field("szukaj_google_opis"))
