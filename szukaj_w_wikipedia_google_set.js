5aaf961c2c38c1d61205d5bc64ef3cefaa229399
wojtish <20014605+wojtish@users.noreply.github.com>
2023-03-03 14:58:07 +0100

    Update szukaj_w_wikipedia_google_set.js

--- szukaj_w_wikipedia_google_set.js
+++ szukaj_w_wikipedia_google_set.js
@@ -1,3 +1,3 @@
 // szukaj_w_wikipedia_google_set.js
 
-entry().set("szukaj_w_wikipedia_google", "https://www.google.com/search?q=" + entry().field("nazwa") + " " + "wikipedia"))
+entry().set("szukaj_w_wikipedia_google", "https://www.google.com/search?q=" + entry().field("nazwa") + " " + "wikipedia")
